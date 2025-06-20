a8e8b1d7ea3c81f6da78e84caf7e98d4230c1ee0
Muhammad Falih Rosyidi <falihrosyidi@gmail.com>
2025-06-20 16:35:29 +0700

    -fix .h manageData

--- manageData.h
+++ manageData.h
@@ -1,7 +1,16 @@
 #ifndef MANAGEDATA_H
-
 #define MANAGEDATA_H
 
+typedef struct Dokter Dokter;
+typedef struct Data Data;
 
+Dokter* findDokter(Data* listDokter, int id);
+void addDokter(Data* listDokter, int id, char* nama, int maxShift, char prefShift);
+void manual_addDokter(Data* listDokter);
+void printData(Data* listDokter);
+void editDokter(Data* listDokter);
+void deleteNode(Data* listDokter);
+void collectData(Data* listDokter, char* fileInput);
+void updateData(Data* listDokter, char* namaFile);
 
-#endif // matrix
+#endif // MANAGEDATA_H
