3af680d1de9df128d19a5ec6c2ca53d1ea711ad9
Kaditya Rakan Pandyansa <166594666+CauchyFx@users.noreply.github.com>
2025-06-22 21:58:50 +0700

    Update informasi.h

--- informasi.h
+++ informasi.h
@@ -1,45 +1,27 @@
-#ifndef INFORMASI_H
-#define INFORMASI_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "manageData.h"
+#include "buatjadwal.h"
+
 #define MAX_DOKTER 100
 #define MAX_HARI 30
-#define SHIFT_PER_HARI 3 // Pagi, Siang, Malam
+#define SHIFT_PER_HARI 3
+#define DOKTER_PER_SHIFT 3
+#define JUMLAH_HARI_JADWAL MAX_HARI
 #define HARI_PER_MINGGU 7
 
-// Struktur Dokter
 typedef struct {
-    int id;
-    char nama[50];
-    int max_shift_per_minggu;
-    char preferensi_shift; // 'P', 'S', 'M'
-} Dokter;
-
-// Struktur Shift dan Hari
-typedef struct {
-    int id_dokter;
-    char shift; // 'P', 'S', 'M'
+    int id_dokter[DOKTER_PER_SHIFT];
+    char shift;
 } Shift;
 
 typedef struct {
     Shift shift[SHIFT_PER_HARI];
 } Hari;
 
-// Struktur Statistik untuk tracking shift per dokter
-typedef struct {
-    int total_shift;
-    int pelanggaran_preferensi;
-} StatistikDokter;
-
-// Inisialisasi dokter dummy
-void inisialisasi_dokter(Dokter dokter[], int *jumlah);
-
-void buat_jadwal_otomatis(Dokter dokter[], int jumlah_dokter, Hari jadwal[], int jumlah_hari);
-
+int get_index_dokter(DataDokter data_dokter[], int jumlah, int id);
 void tampilkan_jadwal_harian(Dokter dokter[], int jumlah_dokter, Hari jadwal[], int hari);
-
 void tampilkan_jadwal_mingguan(Dokter dokter[], int jumlah_dokter, Hari jadwal[], int minggu);
-
-void tampilkan_pelanggaran_preferensi(Dokter dokter[], int jumlah_dokter, StatistikDokter statistik[]);
-
-void hitung_pelanggaran_preferensi(Hari jadwal[MAX_HARI], Dokter dokter[], StatistikDokter statistik[], int jumlah_dokter);
-
-#endif
+void tampilkan_jadwal_bulanan(Dokter dokter[], int jumlah_dokter, Hari jadwal[]);
+void tampilkan_pelanggaran(DataDokter data_dokter[], int jumlah_dokter);
