9ea6d0829dd67b875318e71c331f49e20f12f04a
Fatur165 <168172507+Fatur165@users.noreply.github.com>
2025-06-22 08:50:47 +0700

    Update buatjadwal.h
    pergantian data dokter ke dokter

--- buatjadwal.h
+++ buatjadwal.h
@@ -15,25 +15,25 @@ typedef struct {
     char nama[30];
     int maks_shift_per_minggu;
     int preferensi_shift[JUMLAH_SHIFT_PER_HARI];
-} DataDokter;
-
-typedef struct {
-    DataDokter data;
-    int total_shift_terjadwal;
-    int shift_mingguan_terjadwal[JUMLAH_HARI_JADWAL / 7 + 1];
 } Dokter;
 
 typedef struct {
-    Dokter *dokter_bertugas[JUMLAH_SHIFT_PER_HARI][NDktrperShift];
+    Dokter data;
+    int total_shift_terjadwal;
+    int shift_mingguan_terjadwal[JUMLAH_HARI_JADWAL / 7 + 1];
+} DataDokter;
+
+typedef struct {
+    DataDokter *dokter_bertugas[JUMLAH_SHIFT_PER_HARI][NDktrperShift];
 } ShiftHarian;
 
-extern Dokter dokter[NDokterMax];
+extern DataDokter dokter[NDokterMax];
 extern int jumlah_dokter;
 extern int status_langgar[JUMLAH_HARI_JADWAL][JUMLAH_SHIFT_PER_HARI][NDktrperShift];
 
 void reset_jadwal(ShiftHarian jadwal[]);
-int periksa_valid(Dokter *d, int hari, int tipe_shift, int posisi_dalam_shift, ShiftHarian jadwal[]);
-int hitung_skor(Dokter *d, int tipe_shift, int hari);
+int periksa_valid(DataDokter *d, int hari, int tipe_shift, int posisi_dalam_shift, ShiftHarian jadwal[]);
+int hitung_skor(DataDokter *d, int tipe_shift, int hari);
 int buatJadwal(int hari, int tipe_shift, int shiftKe, ShiftHarian jadwal[]);
 void lihatJadwal(ShiftHarian jadwal[]);
 
